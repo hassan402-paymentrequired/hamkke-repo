8f38f2f5f8e63e7ebe144e8b44d4a74f5be95110
Victoria Etim <vkeylicious@gmail.com>
2023-12-24 01:33:22 +0100

    Authentication with Laravel Breeze
    - Install and Setup Laravel Breeze
    - Add the assets(js, css and image files) for authenticated and guest admin layout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::directive('form_field_error', function ($formFieldName) {
+            return "<?php if (\$errors->has({$formFieldName})): ?>
+                <span class=\"help-block form-error\"  role=\"alert\">
+                    <?php echo \$errors->first({$formFieldName}); ?>
+                </span>
+            <?php endif; ?>";
+        });
+        Blade::directive('old_selected', function ($expression) {
+            $arguments = explode(',', $expression);
+            $formFieldName = $arguments[0];
+            $optionValue = $arguments[1];
+            $optionText = !empty($arguments[2]) ? $arguments[2] : $optionValue;
+            return "<option value={$optionValue}" . (old($formFieldName) == $optionValue ? 'selected' : '') . ">{$optionText}</option>";
+        });
     }
 }
